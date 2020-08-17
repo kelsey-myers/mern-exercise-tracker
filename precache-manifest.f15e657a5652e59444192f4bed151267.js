a3784963269cb28997914f653d50adc26684f42e
Kelsey Myers <kelseymyerss@gmail.com>
2020-08-17 19:22:47 +0100

    Updates

---
+++ precache-manifest.f15e657a5652e59444192f4bed151267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "101c93d27b77864a9c1f",
+    "url": "/mern-exercise-tracker/static/js/main.dc6f8279.chunk.js"
+  },
+  {
+    "revision": "1a35910eb7df83e65491",
+    "url": "/mern-exercise-tracker/static/js/runtime~main.5ae7a0cb.js"
+  },
+  {
+    "revision": "d28e90fb73fe2be0730b",
+    "url": "/mern-exercise-tracker/static/css/2.93162a61.chunk.css"
+  },
+  {
+    "revision": "d28e90fb73fe2be0730b",
+    "url": "/mern-exercise-tracker/static/js/2.83ac70b8.chunk.js"
+  },
+  {
+    "revision": "07486dc64c287f3a01222fc9dfc3dd0b",
+    "url": "/mern-exercise-tracker/index.html"
+  }
+];
